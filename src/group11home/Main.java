698aee0affb1808f093be3d3a78100335ff48a61
nobuyoshi24 <nobuyoshi2426@gmail.com>
2015-10-31 17:23:41 +0900

    First commit

---
+++ src/group11home/Main.java
@@ -0,0 +1,13 @@
+package group11home;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+
+public class Main extends Robot {
+
+	public Main() throws AWTException {
+		super();
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
---
+++ src/group11home/Sub1.java
@@ -0,0 +1,5 @@
+package group11home;
+
+public class Sub1 {
+
+}
