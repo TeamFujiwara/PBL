ad28730f6c82b57f2ddff09ea49a791f8146ac1a
nobuyoshi24 <nobuyoshi2426@gmail.com>
2016-01-20 11:11:48 +0900

    間違って消してしまったものを復元

---
+++ group11home/Sub1.java
@@ -0,0 +1,40 @@
+package group11home;
+import java.awt.Color;
+import java.awt.geom.*;
+import robocode.*;
+import java.util.*;
+
+/*
+ * ソースコードについて
+ * 文字コード... UTF-8
+ * 変数を新しく作るときは
+ * int direction;	//敵の向き
+ * みたいにコメントを付けておいてください
+ *
+ * 11/27追記
+ * 途中になってる部分は //TODO コメント
+ * っていうコメントをつけていおいてください
+ * 例 ↓
+ */
+
+//TODO: 別々の人が作ってる関数内で共有できる変数がいくつかあるので統合する
+//TODO: 標的の共有をする
+//TODO	射撃を実装
+//
+
+/**
+ * ロボット本体のソースコード
+ * レーダーからわかる敵の情報はhttp://www.solar-system.tuis.ac.jp/Java/robocode_api/を参照
+ * 	→ScannedRobotEventクラスに保存される
+ */
+public class Sub1 extends Group11Robot{
+
+	public Sub1() {
+		super.whoAmI = 2;
+		super.robotColor = Color.blue;
+		super.gunColor = Color.green;
+		super.radarColor = Color.magenta;
+	}
+
+	
+}
